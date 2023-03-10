607ac7fd54e0178e796616ff5000561a42776309
Kyle Tan <120530858+ktan46@users.noreply.github.com>
2023-03-10 16:51:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-
+let i = 0;
+let txt = "I'm Kyle Tan, a 2B Honours Mathematics student studying at the University of Waterloo, as well as an aspiring software developer with an interest in machine learning!"; /* The text */
+let speed = 65; /* lower the faster */
+function typeWriter() {
+  if (i < txt.length) {
+  document.getElementById("intro").innerHTML += txt.charAt(i);
+  i++;
+  setTimeout(typeWriter, speed);
+  }
+}
+typeWriter();
