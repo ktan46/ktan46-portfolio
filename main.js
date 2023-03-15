9eed4cd7ba40d35ac0f0d9a554bd2a9b40103dcb
Kyle Tan <120530858+ktan46@users.noreply.github.com>
2023-03-15 13:45:12 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let i = 0;
-let txt = "I'm Kyle Tan, a 2B Honours Mathematics student studying at the University of Waterloo, as well as an aspiring software developer with an interest in machine learning!"; /* The text */
+let txt = "I'm Kyle Tan, a 2B Honours Mathematics student studying at the University of Waterloo and I like computers. /* The text */
 let speed = 65; /* lower the faster */
 function typeWriter() {
   if (i < txt.length) {
